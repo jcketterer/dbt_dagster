f0afd90c3e4fa871c3afc25b86eeefa68918adac
JC Ketterer <jcketterer18@gmail.com>
2024-09-27 21:01:06 -0700

    adding new dagster orchestration project

--- dbt_dagster_project/dbt_dagster_project/constants.py
+++ dbt_dagster_project/dbt_dagster_project/constants.py
@@ -3,11 +3,7 @@ from pathlib import Path
 
 from dagster_dbt import DbtCliResource
 
-dbt_project_dir = (
-    Path(__file__)
-    .joinpath("..", "..", "..", "dbtlearn")
-    .resolve()
-)
+dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "dbtlearn").resolve()
 dbt = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
 
 # If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at run time.
